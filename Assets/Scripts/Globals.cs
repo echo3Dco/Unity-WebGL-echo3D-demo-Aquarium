1a5cde31aa7bbd172af37ac45b7a8da0e297b5b3
andres <andres@echo3d.co>
2023-03-09 17:34:49 -0500

    Add built app for github pages

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,29 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Globals : MonoBehaviour
 {
-    public const string fishProjectApiKey = "white-bar-9326";
-    public const string fishProjectSecKey = "AuvZCWLkWd52HQksar4pNuv7";
-    public const string staticModelsProjectApiKey = "autumn-hat-6692";
-    public const string staticModelsProjectSecKey = "9kmDz87DfJbdHcC4CjRF8eSZ";
-    public const string hammerheadEntryId = "ac0aa4a9-5b17-48ca-8273-786b7b0b9d8b"; //The hammerhead requires some special handling
+    public const string fishProjectApiKey = "";
+    public const string fishProjectSecKey = "";
+    public const string staticModelsProjectApiKey = "";
+    public const string staticModelsProjectSecKey = "";
+    public const string hammerheadEntryId = "";
 
     //Static aquarium decorations (coral, rock, chest)
-    public const string arch01EntryId = "1bf52dd8-b138-4363-adc3-c17f00af69e3";
-    public const string largeSoftCoralEntryId = "ff7d3d20-1483-4951-a6e6-9a1859305a1b";
-    public const string smallSoftCoralEntryId = "78263cc9-8802-4856-abbc-562d37ed3213";
-    public const string bulbousCoralEntryId = "94495824-fb94-4144-bb01-d7281ed09b51";
-    public const string blobbyCoralEntryId = "9547a4c3-f3c5-41dd-bce2-acf75e2a21c8";
-    public const string largeShelfCoral2EntryId = "0c59dae5-af4a-4eb5-ba3e-bffaeab7b778";
-    public const string coral3EntryId = "9911f1c4-8ea4-4a7e-8901-17ccd9e94427";
-    public const string coralBunch1EntryId = "9128bbe9-8e34-4122-ac5d-b11b444ac5b7";
-    public const string coralBunch3EntryId = "f022382a-6e5d-463e-9d4c-445743cf62ae";
-    public const string largeShelfCoral4EntryId = "8f79d30e-0f4b-4bc4-9366-c46e1d0c207e";
-    public const string chestTrunkEntryId = "0ef5dad7-77d3-4a5f-99f8-e1c12c3e7e13";
-    public const string chestLidEntryId = "378052bb-b8df-43ee-9cb3-9bdba5f16303";
-    public const string chestGoldEntryId = "023e5694-74e4-42c4-8bd3-333b6a7f1c8d";
+    public const string arch01EntryId = "";
+    public const string largeSoftCoralEntryId = "";
+    public const string smallSoftCoralEntryId = "";
+    public const string bulbousCoralEntryId = "";
+    public const string blobbyCoralEntryId = "";
+    public const string largeShelfCoral2EntryId = "";
+    public const string coral3EntryId = "";
+    public const string coralBunch1EntryId = "";
+    public const string coralBunch3EntryId = "";
+    public const string largeShelfCoral4EntryId = "";
+    public const string chestTrunkEntryId = "";
+    public const string chestLidEntryId = "";
+    public const string chestGoldEntryId = "";
 
 
 }
