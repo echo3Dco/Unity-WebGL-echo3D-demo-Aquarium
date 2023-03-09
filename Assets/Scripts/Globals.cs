102ccd65ca82c4f85e20c86296eab74f3626275a
andres-echo <101141358+andres-echo@users.noreply.github.com>
2023-03-09 17:16:28 -0500

    Update SDK & Unity Version
    Update project to new SDK and Unity 2021 LTS
    
    Co-authored-by: andres <andres@echo3d.co>

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Globals : MonoBehaviour
-{   
-    public const string fishProjectApiKey = "<FISH PROJECT API KEY HERE>";
-    public const string fishProjectSecKey = "<FISH PROJECT SEC KEY HERE>";
-    public const string staticModelsProjectApiKey = "<STATIC MODEL PROJECT API KEY HERE>";
-    public const string staticModelsProjectSecKey = "<STATIC MODEL PROJECT SEC KEY HERE>";
-    public const string hammerheadEntryId = "<Hammerhead Entry ID>"; //The hammerhead requires some special handling
+{
+    public const string fishProjectApiKey = "white-bar-9326";
+    public const string fishProjectSecKey = "AuvZCWLkWd52HQksar4pNuv7";
+    public const string staticModelsProjectApiKey = "autumn-hat-6692";
+    public const string staticModelsProjectSecKey = "9kmDz87DfJbdHcC4CjRF8eSZ";
+    public const string hammerheadEntryId = "ac0aa4a9-5b17-48ca-8273-786b7b0b9d8b"; //The hammerhead requires some special handling
 
     //Static aquarium decorations (coral, rock, chest)
-    public const string arch01EntryId = "<Rock Arch Entry ID>";
-    public const string largeSoftCoralEntryId = "<Large Soft Coral Entry ID>";
-    public const string smallSoftCoralEntryId = "<Small Soft Coral Entry ID>";
-    public const string bulbousCoralEntryId = "<Bulbous Coral Entry ID>";
-    public const string blobbyCoralEntryId = "<Blobby Coral Entry ID>";
-    public const string largeShelfCoral2 = "<Large Shelf Coral 2 Entry ID>";
-    public const string coral3 = "<Coral3 Entry ID>";
-    public const string coralBunch1 = "<Coral Bunch 1 Entry ID>";
-    public const string coralBunch3 = "<Coral Bunch3 EntryID>";
-    public const string largeShelfCoral4 = "<Large Shelf Coral4 Entry ID>";
-    public const string chestTrunk = "<Chest Trunk Entry ID>";
-    public const string chestLid = "<Chest List Entry ID>";
-    public const string chestGold = "<Chest Gold Entry ID>";
+    public const string arch01EntryId = "1bf52dd8-b138-4363-adc3-c17f00af69e3";
+    public const string largeSoftCoralEntryId = "ff7d3d20-1483-4951-a6e6-9a1859305a1b";
+    public const string smallSoftCoralEntryId = "78263cc9-8802-4856-abbc-562d37ed3213";
+    public const string bulbousCoralEntryId = "94495824-fb94-4144-bb01-d7281ed09b51";
+    public const string blobbyCoralEntryId = "9547a4c3-f3c5-41dd-bce2-acf75e2a21c8";
+    public const string largeShelfCoral2EntryId = "0c59dae5-af4a-4eb5-ba3e-bffaeab7b778";
+    public const string coral3EntryId = "9911f1c4-8ea4-4a7e-8901-17ccd9e94427";
+    public const string coralBunch1EntryId = "9128bbe9-8e34-4122-ac5d-b11b444ac5b7";
+    public const string coralBunch3EntryId = "f022382a-6e5d-463e-9d4c-445743cf62ae";
+    public const string largeShelfCoral4EntryId = "8f79d30e-0f4b-4bc4-9366-c46e1d0c207e";
+    public const string chestTrunkEntryId = "0ef5dad7-77d3-4a5f-99f8-e1c12c3e7e13";
+    public const string chestLidEntryId = "378052bb-b8df-43ee-9cb3-9bdba5f16303";
+    public const string chestGoldEntryId = "023e5694-74e4-42c4-8bd3-333b6a7f1c8d";
 
 
 }
---
+++ Assets/Scripts/SceneHelper.cs
@@ -0,0 +1,255 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+This script is used to make adjustments to loaded holograms pre and post load.
+It determines the appropriate adjustments based on the gameobject name. 
+*/
+
+[RequireComponent(typeof(Echo3DHologram))]
+public class SceneHelper : MonoBehaviour
+{
+    public bool onLoadAdjustment = false;
+    private Echo3DHologram hologram;
+
+    public Material ArchRockMaterial;
+    public Material SmallSoftCoralMaterial;
+    public Material LargeSoftCoralMaterial;
+    public Material BulbousCoralMaterial;
+    public Material CoralBunchMaterial;
+    public Material LargeShelfCoral2Material;
+    public Material LargeShelfCoral4Material;
+    public Material BlobbyCoralMaterial;
+
+    void Awake()
+    {
+        hologram = GetComponent<Echo3DHologram>();
+        if (hologram == null)
+        {
+            Debug.LogError("No Echo3DHologram exists on this gameobject. Scenehelper will not help!");
+            return;
+        }
+        SetApiAndSecurityKeys(hologram);
+        SetEntry(hologram);
+    }
+
+
+    private void SetApiAndSecurityKeys(Echo3DHologram hologram)
+    {
+        switch (gameObject.name)
+        {
+            case "ArchRock":
+            case "SmallSoftCoral":
+            case "LargeSoftCoral":
+            case "BulbousCoral":
+            case "CoralBunch1":
+            case "CoralBunch3":
+            case "LargeShelfCoral2":
+            case "LargeShelfCoral4":
+            case "BlobbyCoral":
+            case "Coral3":
+            case "ChestTrunk":
+            case "ChestGold":
+            case "ChestLid":
+                {
+                    hologram.apiKey = Globals.staticModelsProjectApiKey;
+                    hologram.secKey = Globals.staticModelsProjectSecKey;
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    private void SetEntry(Echo3DHologram hologram)
+    {
+        switch (gameObject.name)
+        {
+            case "ArchRock":
+                {
+                    hologram.entries = Globals.arch01EntryId;
+                    break;
+                }
+            case "SmallSoftCoral":
+                {
+                    hologram.entries = Globals.smallSoftCoralEntryId;
+                    break;
+                }
+            case "LargeSoftCoral":
+                {
+                    hologram.entries = Globals.largeSoftCoralEntryId;
+                    break;
+                }
+            case "BulbousCoral":
+                {
+                    hologram.entries = Globals.bulbousCoralEntryId;
+                    break;
+                }
+            case "CoralBunch1":
+                {
+                    hologram.entries = Globals.coralBunch1EntryId;
+                    break;
+                }
+            case "CoralBunch3":
+                {
+                    hologram.entries = Globals.coralBunch3EntryId;
+                    break;
+                }
+            case "LargeShelfCoral2":
+                {
+                    hologram.entries = Globals.largeShelfCoral2EntryId;
+                    break;
+                }
+            case "LargeShelfCoral4":
+                {
+                    hologram.entries = Globals.largeShelfCoral4EntryId;
+                    break;
+                }
+            case "BlobbyCoral":
+                {
+                    hologram.entries = Globals.blobbyCoralEntryId;
+                    break;
+                }
+            case "Coral3":
+                {
+                    hologram.entries = Globals.coral3EntryId;
+                    break;
+                }
+            case "ChestGold":
+                {
+                    hologram.entries = Globals.chestGoldEntryId;
+                    break;
+                }
+            case "ChestLid":
+                {
+                    hologram.entries = Globals.chestLidEntryId;
+                    break;
+                }
+            case "ChestTrunk":
+                {
+                    hologram.entries = Globals.chestTrunkEntryId;
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    private void OnLoadAdjustment()
+    {
+        MeshRenderer myRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
+        switch (gameObject.name)
+        {
+            case "ArchRock":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = ArchRockMaterial;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "SmallSoftCoral":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = SmallSoftCoralMaterial;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "LargeSoftCoral":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = LargeSoftCoralMaterial;
+                        myRenderer.material.SetFloat("_FlutterAmount", 0.002f);
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "BulbousCoral":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = BulbousCoralMaterial;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "CoralBunch1":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = CoralBunchMaterial;
+                        onLoadAdjustment = false;
+                    };
+                    break;
+                }
+            case "CoralBunch3":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = CoralBunchMaterial;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "LargeShelfCoral2":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = LargeShelfCoral2Material;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "LargeShelfCoral4":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = LargeShelfCoral4Material;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "BlobbyCoral":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = BlobbyCoralMaterial;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            case "Coral3":
+                {
+                    if (myRenderer)
+                    {
+                        myRenderer.material = BlobbyCoralMaterial;
+                        onLoadAdjustment = false;
+                    }
+                    break;
+                }
+            default:
+                {
+                    onLoadAdjustment = false;
+                    break;
+                }
+        }
+    }
+
+    void Update()
+    {
+        if (onLoadAdjustment)
+        {
+            OnLoadAdjustment();
+        }
+    }
+
+}
